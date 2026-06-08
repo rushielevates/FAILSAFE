d5cf7ca07d177bbc786c7bbcb2e5509e1106d5ea
Rushi <rushiprasadbadgar@gmail.com>
2026-06-08 17:56:23 +0530

    Fix CORS for production

--- failsafe-backend/main.py
+++ failsafe-backend/main.py
@@ -23,7 +23,9 @@ def startup_event():
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5173",
+        "https://failsafe-six.vercel.app"
+        ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
